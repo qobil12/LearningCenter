e5fdeaf78e6635949ef08e30c17cae08e1a082a8
Qobiljon <98233535+qobil12@users.noreply.github.com>
2023-03-18 16:44:29 +0500

    Initial commit

---
+++ LearningCenter/src/main/java/com/company/LearningCenterApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningCenterApplication.class, args);
+	}
+
+}
